60e6d36d623a4a399ed238f9645de7067f805733
Anirudh Kumar <anirudh9123@gmail.com>
2026-05-20 11:12:49 +0530

    Updated project1.java

--- project1.java
+++ project1.java
@@ -1,51 +1,74 @@
+```java
 class FoodItem {
-    private String itemName;
-    private double price;
 
-    public FoodItem(String itemName, double price) {
-        this.itemName = itemName;
-        this.price = price;
+    private String foodName;
+    private double itemPrice;
+
+    public FoodItem(String foodName, double itemPrice) {
+        this.foodName = foodName;
+        this.itemPrice = itemPrice;
     }
 
-    public String getItemName() {
-        return itemName;
+    public String getFoodName() {
+        return foodName;
     }
 
-    public double getPrice() {
-        return price;
+    public double getItemPrice() {
+        return itemPrice;
     }
 }
 
 class Restaurant {
-    private String restaurantName;
-    private FoodItem[] menu;
 
-    public Restaurant(String restaurantName, FoodItem[] menu) {
-        this.restaurantName = restaurantName;
-        this.menu = menu;
+    private String nameOfRestaurant;
+    private FoodItem[] foodMenu;
+
+    public Restaurant(String nameOfRestaurant, FoodItem[] foodMenu) {
+        this.nameOfRestaurant = nameOfRestaurant;
+        this.foodMenu = foodMenu;
     }
 
-    public void displayMenu() {
-        System.out.println("=== Menu of " + restaurantName + " ===");
-        for (int i = 0; i < menu.length; i++) {
-            if (menu[i] != null) {
-                System.out.println((i + 1) + ". " + menu[i].getItemName() + " - ₹" + menu[i].getPrice());
+    public void showMenu() {
+
+        System.out.println("===== Welcome to " + nameOfRestaurant + " =====");
+
+        for (int i = 0; i < foodMenu.length; i++) {
+
+            FoodItem item = foodMenu[i];
+
+            if (item != null) {
+
+                System.out.println(
+                        (i + 1) + ". " +
+                        item.getFoodName() +
+                        " --> ₹" +
+                        item.getItemPrice()
+                );
             }
         }
-        System.out.println("===============================");
+
+        System.out.println("-----------------------------------");
     }
 }
+
 public class project1 {
+
     public static void main(String[] args) {
+
         FoodItem item1 = new FoodItem("Paneer Tikka", 250.0);
         FoodItem item2 = new FoodItem("Butter Naan", 50.0);
         FoodItem item3 = new FoodItem("Dal Makhani", 200.0);
         FoodItem item4 = new FoodItem("Chocolate Ice Cream", 80.0);
 
         FoodItem[] menu = {item1, item2, item3, item4};
-        Restaurant restaurant = new Restaurant("Taste of India", menu);
 
-        restaurant.displayMenu();
+        Restaurant restaurant =
+                new Restaurant("Taste of India", menu);
+
+        restaurant.showMenu();
+
         System.out.println();
+        System.out.println("Visit Again :)");
     }
 }
+```
